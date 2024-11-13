89f00df0cb089daa9b34017991e1ecda48fef3f8
unknown <maazashrar478@gmail.com>
2024-11-13 16:40:51 +0530

    my 10th commit

--- main.tf
+++ main.tf
@@ -6,10 +6,19 @@
   }
 } */
 
-resource "aws_vpc" "vpcdemo" {
+/* resource "aws_vpc" "vpcdemo" {
   cidr_block = "10.120.0.0/16"
 
   tags = {
     Name = "demo vpc for github"
   }
+} */
+
+resource "aws_instance" "ec2user12" {
+    ami = "ami-06b21ccaeff8cd686"
+    instance_type = "t2.micro"
+    tags = {
+        Name = "user12ec2"
+    }
+  
 }
